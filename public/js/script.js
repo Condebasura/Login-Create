0a833729e113da42295263bfe51b4ff40abcdf73
Pablo Zabala <could_2032@outlook.com>
2024-02-18 03:56:20 -0300

    creando la interfaz del usuario para el su sesion

--- public/js/script.js
+++ public/js/script.js
@@ -40,40 +40,7 @@ form.addEventListener("submit", (e) =>{
     };
 
     verifiDatos(email.value , pass.value);
+
     
 })
 
-/*let body = document.querySelector("body");
-const form = document.querySelector(".form");
-let email = document.querySelector(".e-mail");
-let pass = document.querySelector(".contraseña");
-const btnLogin = document.querySelector(".login");
-const btnRegistro = document.querySelector(".registro");
-
-form.addEventListener("submit", async (e) => {
-    e.preventDefault();
-
-    try {
-        // Realiza una solicitud POST para verificar las credenciales
-        const response = await fetch("http://localhost:3000", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify({ email: email.value, pass: pass.value }),
-        });
-
-        const data = await response.json();
-
-        if (response.ok) {
-            // Si las credenciales son correctas, redirige al usuario a la página de inicio de sesión
-            
-            window.location.href = "/log-in";
-        } else {
-            // Muestra un mensaje de error en caso de credenciales incorrectas
-            document.querySelector(".parrafo").innerHTML = data.mensaje;
-        }
-    } catch (error) {
-        console.error("Error de red:", error);
-    }
-});*/
